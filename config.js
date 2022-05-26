ee09f49b42543583eec3b4dbae5301efeeb5547a
xcgdCreeper <57204065+xcgdCreeper@users.noreply.github.com>
2022-05-26 11:02:30 +0800

    2022.5.26

---
+++ config.js
@@ -0,0 +1,14 @@
+//游戏结束音效
+var soundList=['./cat/cat gasp1.ogg','./cat/cat gasp2.ogg','./cat/cat gasp3.ogg','./cat/cat medium meow.ogg','./cat/cat_meow-mouthy.ogg'];
+//吃到食物音效
+var eatList=['./cat/cat annoyed 1.ogg','./cat/cat annoyed 2.ogg','./cat/cat growl.ogg','./cat/cat_meow-mid.ogg','./cat/cat-meowannoyed.ogg'];
+//游戏背景音乐
+var music='./bgm.mp3';
+//战败提示
+var loseText="猫猫死了";
+//战败图片
+var losePic="./lose.png";
+//地图大小
+var rowCount = 10, colCount = 10;
+//游戏速度 ms/帧
+var speed = 250;
